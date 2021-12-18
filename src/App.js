7aa81798869fcd68a6b3eba1b0fe7d9007fabb2d
scott <sktatsuno@gmail.com>
2021-12-18 00:34:56 -0800

    pivoting to csv, having trouble with apis

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Map from './Map.js';
-import StockForm from './StockForm.js';
 
 const App = () => {
     const [viewport, setViewport] = React.useState({
@@ -9,6 +8,20 @@ const App = () => {
         zoom: 3,
         pitch: 50
     })
+    const [holdings, setHoldings] = React.useState([{
+        "symbol": "AAPL",
+        "weight": 0.75,
+        "latitude": 37.3346,
+        "longitude": -122.0090,
+        "icon_url": "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+       },
+       {
+        "symbol": "MSFT",
+        "weight": 0.25,
+        "latitude": 39.9683,
+        "longitude": -75.2238,
+        "icon_url": "https://i1.wp.com/eodhistoricaldata.com/financial-apis-blog/wp-content/uploads/2019/04/MSFT.png",
+       }])
     const mapboxApiAccessToken = "pk.eyJ1Ijoic3RhdHN1bm8iLCJhIjoiY2t4NnUwdWRlMGk5cDJ2cW0xNzI3eXJjcCJ9.P2MekVGXfEld3SmY1-6dlw"
     const mapStyle="mapbox://styles/mapbox/light-v10"
     return (
@@ -20,8 +33,10 @@ const App = () => {
             onViewportChange={nextViewport => setViewport(nextViewport)}
             mapStyle={mapStyle}
             mapboxApiAccessToken={mapboxApiAccessToken}
+            holdings={holdings}
             />
-            <StockForm></StockForm>
+            {/* <StockForm></StockForm> */}
+
         </div>
         
     );
--- src/Map.js
+++ src/Map.js
@@ -3,42 +3,14 @@ import ReactMapGL, {Marker} from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
 import { ColumnLayer, IconLayer } from '@deck.gl/layers';
 
-const Map = ({ width, height, viewport, onViewportChange, mapStyle, mapboxApiAccessToken}) => {
+const Map = ({ width, height, viewport, onViewportChange, mapStyle, mapboxApiAccessToken, holdings}) => {
     const [hoverInfo, setHoverInfo] = React.useState("");
     const [onClickInfo, setOnClickInfo] = React.useState("");
 
-    const sourceData = [{
-        "icon_url": "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
-        "incident_id": 893251,
-        "date": "7/1/2013",
-        "n_killed": 50,
-        "n_injured": 1,
-        "latitude": 37.3346,
-        "longitude": -122.0090,
-        "location": 0,
-        "notes": "Traffic stop; backed car into approaching officer  knocked him to ground",
-        "categories": "Shot - Wounded/Injured||Officer Involved Incident||Officer Involved Shooting - subject/suspect/perpetrator shot||Drug involvement",
-        "message": "AAPL 57%",
-        "symbol": "AAPL"
-       },
-       {
-        "icon_url": "https://i1.wp.com/eodhistoricaldata.com/financial-apis-blog/wp-content/uploads/2019/04/MSFT.png",
-        "incident_id": 964582,
-        "date": "11/22/2013",
-        "n_killed": 20,
-        "n_injured": 1,
-        "latitude": 39.9683,
-        "longitude": -75.2238,
-        "location": 0,
-        "notes": "Pit gang suspect fired into rival Grounds territory  hit bystander",
-        "categories": "Shot - Wounded/Injured||Gang involvement",
-        "message": "MSFT 43%",
-        "symbol": "MSFT"
-       }];
     const layers = [
         new IconLayer({
             id: 'icon-layer',
-            data: sourceData,
+            data: holdings,
             // iconAtlas and iconMapping should not be provided
             // getIcon return an object which contains url to fetch icon of each data point
             getIcon: d => ({
@@ -61,12 +33,12 @@ const Map = ({ width, height, viewport, onViewportChange, mapStyle, mapboxApiAcc
         }),
         new ColumnLayer({
             id: 'column',
-            data: sourceData,
+            data: holdings,
             getPosition: d => [d.longitude, d.latitude],
-            getElevation: d => (d.n_killed * 2) + d.n_injured,
+            getElevation: d => d.weight * 100,
             getFillColor: d => [135,206,235],
             getLineColor: [0, 0, 0],
-            elevationScale: 30000,
+            elevationScale: 50000,
             extruded: true,
             radius: 15000 * (20/viewport.zoom),         
             opacity: 0.2,        
@@ -90,7 +62,7 @@ const Map = ({ width, height, viewport, onViewportChange, mapStyle, mapboxApiAcc
           layers={layers} >
           {hoverInfo.object && (
             <div className="tooltip" style={{position: 'absolute', zIndex: 1, pointerEvents: 'none', left: hoverInfo.x + 25, top: hoverInfo.y + 25, fontFamily: 'arial'}}>
-              { hoverInfo.object.message }
+              {`${hoverInfo.object.symbol} ${hoverInfo.object.weight}`}
             </div>
           )}
           {onClickInfo.object && (
